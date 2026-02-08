569bcc22bf7868898f7a6ad6ee30808c4f8021f2
Arijeet Basu <2105845@reva.edu.in>
2026-02-08 18:39:17 +0530

    typos

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -15,8 +15,10 @@ export default function TextFooter() {
       <h1
         className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
       >
-        <span className="text-gray-400">Haiii <br/> My Anna! <br/> Match</span> <br /> the photo pairs
+        <span className="text-gray-400"> Haiii <br/> My Anna! </span> <br/> Match <br/> the photo pairs
+
       </h1>
+
 
       {/* Right Text */}
       <h1
